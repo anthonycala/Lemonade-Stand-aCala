6272aba42078e75c033df56167d52d62a06c3298
unknown <antcala@icloud.com>
2019-10-03 10:31:44 -0500

    finished Day.cs

--- Lemonade_Stand/Day.cs
+++ Lemonade_Stand/Day.cs
@@ -11,19 +11,43 @@ namespace Lemonade_Stand
         //member variables (Has A)
         public Weather weather;
         public List<Customer> customers;
-
         public string DayOfWeek;
 
         // Constructor
         public Day(string dayOfWeek)
         {
             DayOfWeek = dayOfWeek;
+            weather = new Weather();
+            customers = new List<Customer>();
+            createCustomer(weather);
         }
 
-
-
-
-
         // member methods
+        public void createCustomer(Weather PastInWeather)
+        {
+            int numOfCustomers = 0;
+            if(PastInWeather.Condition == "Sunny")
+            {
+                numOfCustomers = 30;
+            }
+            else if(PastInWeather.Condition == "Cloudy")
+            {
+                numOfCustomers = 20;
+            }
+            else if(PastInWeather.Condition == "Rainy")
+            {
+                numOfCustomers = 10;
+            }
+            else if(PastInWeather.Condition == "Foggy")
+            {
+                numOfCustomers = 5;
+            }
+            for (int i = 0; i < numOfCustomers; i++)
+
+            {
+                customers.Add(new Customer());
+
+            }
+        }
     }
 }
