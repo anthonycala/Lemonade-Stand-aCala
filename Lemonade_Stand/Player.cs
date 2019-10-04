b33674f8e50addd63d61f6c6176f09afb4a315a5
unknown <antcala@icloud.com>
2019-10-04 16:11:44 -0500

    wrote member method for adjust lemons, sugarcubes, ice cubes, cups, and then adjusted with the inventory based on used

--- Lemonade_Stand/Player.cs
+++ Lemonade_Stand/Player.cs
@@ -43,7 +43,53 @@ namespace Lemonade_Stand
             name = Console.ReadLine();
             return name;
         }
+        public int pourPitcher()
+        {
+            Console.WriteLine("How many pitchers do you want to make");
+            int numOfPitchers = int.Parse(Console.ReadLine());
+            return numOfPitchers;
+        }
+        public void adjustInventory(int numOfPitchers)
+        {
+            int usedLemons = numOfPitchers * recipe.amountOfLemons;
+            int usedSugarCubes = numOfPitchers * recipe.amountOfSugarCubes;
+            int usedIceCubes = numOfPitchers * recipe.amountOfIceCubes;
+            int usedCups = numOfPitchers * pitcher.cupsLeftInPitcher;
+            adjustLemons(usedLemons);
+            adjustSugarCubes(usedSugarCubes);
+            adjustIceCubes(usedIceCubes);
+            adjustCupsUsed(usedCups);
+            
+        }
+        public void adjustLemons(int usedLemons)
+        {
+            for (int i = 0; i < usedLemons; i++)
+            {
+                inventory.Lemons.RemoveAt(0);
+            }
 
+        }
+        public void adjustSugarCubes(int usedSugarCubes)
+        {
+            for (int i = 0; i < usedSugarCubes; i++)
+            {
+                inventory.SugarCubes.RemoveAt(0);
+            }
+
+        }
+        public void adjustIceCubes(int usedIceCubes)
+        {
+            for (int i = 0; i < usedIceCubes; i++)
+            {
+                inventory.IceCubes.RemoveAt(0);
+            }
+        }
+        public void adjustCupsUsed(int usedCups)
+        {
+            for (int i = 0; i < usedCups; i++)
+            {
+                inventory.Cups.RemoveAt(0);
+            }
+        }
     }
-
 }
